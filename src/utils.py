7c3ede09f553e37fb9f81e8baac3fea891126891
Corey Sabia <12410796+coreysabia@users.noreply.github.com>
2019-10-24 10:30:12 -0400

    Initial Python files

---
+++ src/utils.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from config.settings import Settings
+
+def list_to_string(to_stringify):
+    # Join lists into a string
+    return
+
+def pipeify(string):
+    return
+
+def next_index(index, direction):
+    return
+
+def remove_empty_character(non_empty_list):
+    # Remove empty characters from a list which 
+    # might have empty characters and return it
+    return
+
+def count_occurrences(in_list):
+    # Return the number of occurrences
+    # in a list which has no empty characters
+    # First clean list with remove_empty_character 
+   return
