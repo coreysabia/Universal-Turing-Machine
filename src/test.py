a796aebd1e6f61344c52405f5ae5029dc273ab28
Matthew Ruch <mruch996@g.rwu.edu>
2019-11-06 09:32:14 -0500

    new utils + updated tests

--- src/test.py
+++ src/test.py
@@ -1,60 +1,13 @@
 # -*- coding: utf-8 -*-
 
-# Set the length of the tape 
-# that is visable to the user
-DISPLAY_TAPE_LENGTH = 20
-
-# Set the allowed tape movements
-TAPE_MOVEMENTS = {'right': 1, 'left': -1}
-
-# For returning an empty character 
-# when needed, maybe we should move 
-# this to utils.py, not sure yet
-EMPTY_CHARACTER = ' '
-
-def tape_movements():
-    return TAPE_MOVEMENTS.keys()
-
-def get_next_direction(direction):
-    return TAPE_MOVEMENTS[direction]
-    
-def empty_char():
-    return EMPTY_CHARACTER
-    
-def display_tape_length():
-    return DISPLAY_TAPE_LENGTH
-
-def stringify(list):
-    # Concatenate lists into string
-    return str.join('', list)
-
-def pipeify(string):
-    return
-
-def next_position(index, direction):
-    next_index = index + get_next_direction(direction)
-    return next_index
-
-def clean_list(list):
-    # Remove empty characters from a list which 
-    # might have empty characters and return it
-    while '' in list:
-        list.remove('')
-    return list
-
-def check_list_length(list): #skips over empty space without cleaning
-    count = 0
-    # Return the number of occurrences
-    # in a list which has no empty characters
-    # First clean list with remove_empty_character
-    clean = clean_list(list)
-    for i in clean:
-         count += 1
-    return count
+from utils import *
 
 a = ["a","b","c","d"]
 b = ["a","b","","c"]
 c = 0
+d = ["a","b"," ","c"]
+e = ["a","b","","c", " "]
+f = [">","b","c","d","X","X","Y","[","a","b","c","]","a","b","c","]","a","b","c","a"] # list has an intentional unkown charater
 
 string = stringify(a)
 print("Stringify test:")
@@ -63,12 +16,29 @@ print("\n")
 
 print("clean test:")
 print(clean_list(b))
+print(clean_list(d))
+print(clean_list(e))
 print("\n")
 
 print("checklist length test:")
 print(check_list_length(b))
+print(check_list_length(f))
 print("\n")
 
 print("next position:")
 print(next_position(c, "right"))
 print("\n")
+
+
+print("count_list_members test:")
+print("the tested list:")
+print(f)
+print("[char_a, char_b, char_c, char_arrow, char_x, char_y, char_left_bracket, char_right_bracket, char_unknown]")
+print(count_list_members(f))
+
+print("count_specific_member test:")
+#prints out the number of a specific charatcer in a list
+#can be passed an upper or lower case charater
+print(count_specific_member(f, "A"))
+#unknown characters are addded up in the function aswell, can use this to check the string is valid with our stirng alphabet
+print(count_specific_member(f, "v"))
